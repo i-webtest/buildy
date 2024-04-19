dc50481f634e75db96b8faec4956b4fc97bc12f0
i-webtest <stasbass80@gmail.com>
2024-04-19 22:49:02 +0300

    Start new project

---
+++ vite.config.js
@@ -0,0 +1,32 @@
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // base: '/buildy/',
+  // base: '/buildy/dist/',
+  // base: '/dist/',
+  root: './src',
+  publicDir: '../public',
+  build: {
+    outDir: '../dist',
+  },
+  plugins: [
+    ViteImageOptimizer({
+      png: {
+        quality: 80,
+      },
+      jpeg: {
+        quality: 80,
+      },
+      jpg: {
+        quality: 80,
+      },
+      webp: {
+        quality: 80,
+      },
+      avif: {
+        quality: 70,
+      },
+    }),
+  ],
+});
